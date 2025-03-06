0765422c2b5057cd8d733cfc5210876e4cc1c189
Saurabh Kumar <129621065+SaurabhKr002@users.noreply.github.com>
2025-03-06 10:22:28 +0530

    Saurabh Add . showing Hello feom Bridgelabz with user inputted name

--- Helloworld_Angular_App/src/app/app.component.ts
+++ Helloworld_Angular_App/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [FormsModule, CommonModule]
+  
 })
 export class AppComponent {
   title = 'Helloworld Angular App';
 
   // Property Binding for Image
   bridgeLabzLogo: string = './assets/BL_logo_square_jpg.jpg';
+
+  // Two-way Data Binding Property
+  userName: string = ''; // Added this line
+
+  // Method to open BridgeLabz website in a new tab
+  openBridgeLabz() {
+    window.open('https://www.bridgelabz.com', '_blank');
+  }
 }
--- Helloworld_Angular_App/src/app/app.module.ts
+++ Helloworld_Angular_App/src/app/app.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { FormsModule } from '@angular/forms'; // Imported FormsModule
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent // Registering HelloWorldComponent
+    HelloWorldComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgModule,
+    CommonModule// Added FormsModule here
   ],
   providers: [],
   bootstrap: [AppComponent]
