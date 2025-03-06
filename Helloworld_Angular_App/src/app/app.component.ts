b18ba631e58b30a90159584568ad3cdd41b16597
Saurabh Kumar <129621065+SaurabhKr002@users.noreply.github.com>
2025-03-06 10:50:32 +0530

    Saurabh Add . Save local changes before merging UC6

--- Helloworld_Angular_App/src/app/app.component.ts
+++ Helloworld_Angular_App/src/app/app.component.ts
@@ -16,12 +16,9 @@ export class AppComponent {
   // Property Binding for Image
   bridgeLabzLogo: string = './assets/BL_logo_square_jpg.jpg';
 
-<<<<<<< HEAD
-=======
   // Two-way Data Binding Property
   userName: string = ''; // Added this line
 
->>>>>>> UC5
   // Method to open BridgeLabz website in a new tab
   openBridgeLabz() {
     window.open('https://www.bridgelabz.com', '_blank');
