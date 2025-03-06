d1b0a5be9ae527baa58584ac3ed14216a58a1bc4
Saurabh Kumar <129621065+SaurabhKr002@users.noreply.github.com>
2025-03-06 10:53:55 +0530

    Saurabh Add . Save local changes before merging UC6

--- Helloworld_Angular_App/src/app/app.component.ts
+++ Helloworld_Angular_App/src/app/app.component.ts
@@ -2,13 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   imports: [FormsModule, CommonModule]
-  
 })
 export class AppComponent {
   title = 'Helloworld Angular App';
@@ -17,10 +15,22 @@ export class AppComponent {
   bridgeLabzLogo: string = './assets/BL_logo_square_jpg.jpg';
 
   // Two-way Data Binding Property
-  userName: string = ''; // Added this line
+  userName: string = ''; 
+  userNameError: string = ''; // Error message
 
   // Method to open BridgeLabz website in a new tab
   openBridgeLabz() {
     window.open('https://www.bridgelabz.com', '_blank');
   }
+
+  // Validate userName
+  validateUserName() {
+    const namePattern = /^[A-Z][a-zA-Z ]{2,}$/; // Allow spaces between names
+    if (!namePattern.test(this.userName)) {
+      this.userNameError = "Name is incorrect! It should start with uppercase and have at least 3 letters.";
+    } else {
+      this.userNameError = ""; // Clear error if valid
+    }
+  }
+  
 }
